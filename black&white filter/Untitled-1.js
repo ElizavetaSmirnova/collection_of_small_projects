1263e2c619f9164a64142433a66e3f46aaa84c28
ElizavetaSmirnova <108726784+ElizavetaSmirnova@users.noreply.github.com>
2023-03-26 19:14:14 +0500

    Add files via upload

---
+++ black&white filter/Untitled-1.js
@@ -0,0 +1,9 @@
+let a = prompt("введите число ");
+let z = Number(a);
+let s = prompt("введите число ");
+let g = Number(s);
+let d = prompt("введите число ");
+let f =  (z + g ) * d;
+
+
+console.log(f)
